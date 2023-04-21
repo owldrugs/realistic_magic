6d182e9feda51243190dc715ff9d5b4251eee86d
Owlondrugs <johndoe@example.com>
2023-04-21 15:02:43 +0300

    first

---
+++ init.lua
@@ -0,0 +1,5 @@
+magic_book = {}
+local mod_name = "realistic_magic"
+local path = minetest.get_modpath(mod_name)
+
+dofile(path.."/unregister.lua")
---
+++ unregister.lua
@@ -0,0 +1,18 @@
+unregister = {}
+
+minetest.unregister_item("default:pick_wood")
+minetest.unregister_item("default:pick_stone")
+
+minetest.unregister_item("default:shovel_wood")
+minetest.unregister_item("default:shovel_stone")
+
+minetest.unregister_item("default:axe_wood")
+minetest.unregister_item("default:axe_stone")
+
+minetest.unregister_item("default:sword_wood")
+minetest.unregister_item("default:sword_stone")
+
+minetest.unregister_item("default:hoe_wood")
+minetest.unregister_item("default:hoe_stone")
+
+return unregister
